efbe005f9e622d2b2def526256a9c2290b93bab2
nikita-petrashen <49820557+nikita-petrashen@users.noreply.github.com>
2019-05-18 13:59:21 +0300

    Add files via upload

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,22 +1,76 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from fastai import *
 from fastai.vision import *
+import os
+IMG_FOLDER = '/static'
 app = Flask(__name__)
-
+app.config['IMG_FOLDER'] = IMG_FOLDER
 learn = load_learner('model/')
 
 @app.route('/upload')
 def upload():
    return render_template('upload.html')
 	
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/result', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
-      f.save(f.filename)
-      img = open_image(f.filename)
+      f.save('static/img.jpg')
+      f.close()
+      img = open_image('static/img.jpg')
       prediction = learn.predict(img)[0]
-      return 'Prediction for ' + f.filename + ' = ' + str(prediction)
-		
+      template = open('templates/result.html', 'w')
+      content = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Result</title>
+    <style>
+
+        .wrapper {
+            height: 600px;
+            position: relative;
+        }
+
+        .container {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+
+        .title {
+            text-align: center;
+            font-size: 30px;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container">
+        <div class="title">
+            <img style="max-width: 600px; height: auto;" src="static/img.jpg" />         
+            <br>
+            prediction = """ + str(prediction) + """
+            
+        </div>
+    </div>
+
+    
+
+</div>
+</body>
+</html>"""
+      template.write(content)
+      template.close()
+
+      resp = make_response(render_template("result.html"))
+      resp.headers['Cache-Control'] =  'no-cache, no-store'
+      resp.headers['Pragma'] = 'no-cache'
+      return resp
+
 if __name__ == '__main__':
    app.run(debug = True)
